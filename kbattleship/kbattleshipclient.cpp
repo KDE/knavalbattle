78db4a916c9ec654f233ba9d2ca0d4418544c7d7
Nikolas Zimmermann <wildfox@kde.org>
2000-11-08 20:29:20 +0000

    Added the KBattleshipClient Class (not yet completed)
    svn path=/trunk/kdegames/kbattleship/; revision=70336

---
+++ kbattleship/kbattleshipclient.cpp
@@ -0,0 +1,54 @@
+/***************************************************************************
+                            kbattleshipclient.cpp
+                             -------------------
+    Developers: (c) 2000 Nikolas Zimmermann <nikoz@gymnasium-kerpen.de>
+                (c) 2000 Daniel Molkentin <molkentin@kde.org>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "kbattleshipclient.moc"
+
+KBattleshipClient::KBattleshipClient( QString host, int port ) : QSocket()
+{
+        internalHost=host;
+        internalPort=port;
+        connect( this, SIGNAL( error( int ) ), SLOT( socketError( int ) ) );
+
+        connect( this, SIGNAL( hostFound() ), SLOT( connectionControl() );
+        connect( this, SIGNAL( connected() ), SLOT( connectionControl() );
+        connect( this, SIGNAL( connectionClosed() ), SLOT( connectionControl() );
+        connect( this, SIGNAL( readyRead() ), SLOT( connectionControl() );
+
+        connectToServer();
+}
+
+KBattleshipClient::~KBattleshipClient()
+{
+}
+
+void KBattleshipClient::connectToServer()
+{
+        connectToHost( host, port);
+}
+
+void KBattleshipClient::connectionControl()
+{
+        kdDebug() << "Client-State: " << state() << endl;
+}
+
+void KBattleshipClient::lostServer()
+{
+}
+
+void KBattleshipClient::socketError( int error )
+{
+}
---
+++ kbattleship/kbattleshipclient.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+                             kbattleshipclient.h
+                             -------------------
+    Developers: (c) 2000 Nikolas Zimmermann <nikoz@gymnasium-kerpen.de>
+                (c) 2000 Daniel Molkentin <molkentin@kde.org>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KBATTLESHIPCLIENT_H
+#define KBATTLESHIPCLIENT_H
+
+#include <stdlib.h>
+#include <kdebug.h>
+#include <qsocket.h>
+#include <qtextstream.h>
+#include "kmessage.h"
+#include "kmessagetype.h"
+
+class KBattleshipClient : QSocket
+{
+    Q_OBJECT
+    public:
+        KBattleshipClient( QString host = "", int port = 54321 );
+        ~KBattleshipClient();
+
+    private slots:
+        void connectionControl();
+        void lostServer();
+        void socketError( int error );
+
+    private:
+        void connectToServer();
+        int internalPort;
+        QString internalHost;
+};
+
+#endif
+
