e48efbcc4b8c86e75127ccc058e1857a2b48e748
Nikolas Zimmermann <wildfox@kde.org>
2000-11-18 23:57:14 +0000

    Adding this new non-working files
    svn path=/trunk/kdegames/kbattleship/; revision=71326

---
+++ kbattleship/kserverdialog.cpp
@@ -0,0 +1,32 @@
+/***************************************************************************
+                              kserverdialog.cpp
+                             -------------------
+    Developers: (c) 2000 Nikolas Zimmermann <nikoz@gymnasium-kerpen.de>
+                (c) 2000 Daniel Molkentin <molkentin@kde.org>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "kserverdialog.moc"
+
+KServerDialog::KServerDialog( QWidget *parent, const char *name ) : startServerDlg( parent, name )
+{
+    connect( startBtn, SIGNAL( clicked() ), SLOT( slotStartClicked() ) );
+}
+
+KServerDialog::~KServerDialog()
+{
+}
+
+void KServerDialog::slotStartClicked()
+{
+    kdDebug() << "START!" << endl;
+}
