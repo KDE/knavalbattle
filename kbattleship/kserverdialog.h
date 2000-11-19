884ede0f63fab758ebd47965ef504447416fe221
Nikolas Zimmermann <wildfox@kde.org>
2000-11-19 00:16:04 +0000

    args that's the shit :/
    svn path=/trunk/kdegames/kbattleship/; revision=71331

--- kbattleship/kserverdialog.h
+++ kbattleship/kserverdialog.h
@@ -1,14 +1,16 @@
 #ifndef KSERVERDIALOG_H
 #define KSERVERDIALOG_H
 
+#include <kdebug.h>
 #include "dialogs/serverDlg.h"
 
-class KServerDialog : public serverStartDlg
+class KServerDialog : startServerDlg
 {
     Q_OBJECT
     public:
         KServerDialog( QWidget *parent = 0, const char *name = 0 );
         ~KServerDialog();
+	
     public slots:
         void slotStartClicked();
 };
