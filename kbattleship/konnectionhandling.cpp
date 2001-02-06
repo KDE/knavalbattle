738903951ee10500be5c8b5c9a30756f1abce2a5
Daniel Molkentin <molkentin@kde.org>
2001-02-06 22:31:28 +0000

    Workaround: Don't die on errors while connecting to other hosts. "emit changeConnectText()" kills it. Dunno why yet.
    svn path=/trunk/kdegames/kbattleship/; revision=81617

--- kbattleship/konnectionhandling.cpp
+++ kbattleship/konnectionhandling.cpp
@@ -197,19 +197,19 @@ void KonnectionHandling::clientSocketError(int error)
         case QSocket::ErrConnectionRefused:
     	    internalClient->kill();
  	    KMessageBox::error(0L, i18n("Connection refused by other host!"));
- 	    emit changeConnectText();
+	// emit changeConnectText();      // FIXME: Don't die
             break;
 
 	case QSocket::ErrHostNotFound:
     	    internalClient->kill();
 	    KMessageBox::error(0L, i18n("Couldn't lookup host!"));
-	    emit changeConnectText();
+	// emit changeConnectText();      // FIXME: Don't die
 	    break;
 	
         case QSocket::ErrSocketRead:	
             internalClient->kill();
  	    KMessageBox::error(0L, i18n("Couldn't connect to server!"));
-	    emit changeConnectText();
+	// emit changeConnectText();      // FIXME: Don't die
 	    break;
 
 	default:
