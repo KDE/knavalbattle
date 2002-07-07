3ed743eb800f8809b89f9e74b48f90285904f218
Stephan Binner <binner@kde.org>
2002-07-07 08:40:24 +0000

    CVS_SILENT Style guide fixes
    svn path=/trunk/kdegames/kbattleship/; revision=165345

--- kbattleship/kbattleship.cpp
+++ kbattleship/kbattleship.cpp
@@ -84,7 +84,7 @@ void KBattleshipApp::initStatusBar()
 
 void KBattleshipApp::initActions()
 {
-	m_gameServerConnect = new KAction(i18n("&Connect to Server"), "connect_no", Key_F2, this, SLOT(slotServerConnect()), actionCollection(), "serverconnect");
+	m_gameServerConnect = new KAction(i18n("&Connect to Server..."), "connect_no", Key_F2, this, SLOT(slotServerConnect()), actionCollection(), "serverconnect");
 	m_gameNewServer = new KAction(i18n("&Start Server"), "network", Key_F3, this, SLOT(slotNewServer()), actionCollection(), "newserver");
 	m_gameSingle = new KAction(i18n("S&ingle Player"), "gear", Key_F4, this, SLOT(slotSinglePlayer()), actionCollection(), "singleplayer");
 	m_gameQuit = KStdAction::quit( this, SLOT(slotGameQuit()), actionCollection(), "gamequit");
