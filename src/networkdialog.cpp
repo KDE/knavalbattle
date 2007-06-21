95b51e558bfce2b0396ad6e0e2c032b20541d349
Paolo Capriotti <p.capriotti@gmail.com>
2007-06-21 15:33:28 +0000

    ebn fixes
    svn path=/trunk/KDE/kdegames/kbattleship/; revision=678511

--- src/networkdialog.cpp
+++ src/networkdialog.cpp
@@ -1,3 +1,13 @@
+/*
+  Copyright (c) 2007 Paolo Capriotti <p.capriotti@gmail.com>
+            (c) 2007 Riccardo Iaconelli <ruphy@fsfe.org>
+            
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
 #include "networkdialog.h"
 
 #include <QGridLayout>
