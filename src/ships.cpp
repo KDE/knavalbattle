dbd5cd5abd2f5057ea0abd477d2b804ee42d471d
Jaime Torres <jtamate@gmail.com>
2014-01-16 20:26:35 +0100

    Add a configuration class to keep a battle configuration
    The class handles the number of ships of each size, their name in
    English, the board size and if adjacent ships are allowed.
    
    Also there are two static methods to return the default battles
    configuration:
    1 ship of size 1,2,3 and 4. (single ship, like it is now in master)
    1 ship of size 4, 2 of size 3, 3 of size 2 and 4 of size 1.

---
+++ src/ships.cpp
@@ -0,0 +1,116 @@
+/*
+  Copyright (c) 2013 Jaime Torres <jtamate@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+#include "ships.h"
+
+Ships::Ships(unsigned int size, unsigned int number, QString& shipsName)
+:m_size(size)
+,m_number(number)
+,m_shipsName(shipsName)
+{
+}
+
+BattleShipsConfiguration::BattleShipsConfiguration()
+: m_ships()
+, m_longestShip(0)
+, m_boardWidth(0)
+, m_boardHeight(0)
+{
+}
+
+
+BattleShipsConfiguration::BattleShipsConfiguration(unsigned int longestShipSize, const bool allowAdjacentShips, const unsigned int boardWidth, const unsigned int boardHeight)
+: m_ships()
+, m_longestShip(longestShipSize)
+, m_boardWidth(boardWidth)
+, m_boardHeight(boardHeight)
+{
+}
+
+BattleShipsConfiguration::BattleShipsConfiguration(const BattleShipsConfiguration& copy)
+: m_ships(copy.m_ships)
+, m_longestShip(copy.longestShip())
+, m_boardWidth(copy.boardWidth())
+, m_boardHeight(copy.boardHeight())
+{
+    
+}
+
+void BattleShipsConfiguration::setLongestShipSize(unsigned int longestShipSize)
+{ 
+    m_longestShip = longestShipSize; 
+    m_ships.reserve(m_longestShip);
+}
+
+BattleShipsConfiguration& BattleShipsConfiguration::addShips(unsigned int size, unsigned int number, QString shipsName)
+{
+    Ships toInsert(size, number, shipsName);
+    m_ships[size]=toInsert;
+    return *this;
+}
+
+BattleShipsConfiguration& BattleShipsConfiguration::addShips(Ships& ships)
+{
+    m_ships[ships.size()]=ships;
+    return *this;
+}
+
+unsigned int BattleShipsConfiguration::numberOfShipsOfSize(unsigned int size) const
+{
+    if ( size > m_longestShip )
+    {
+        return 0;
+    }
+    return m_ships[size].number();
+}
+
+QString BattleShipsConfiguration::nameOfShipsOfSize(unsigned int size) const
+{
+    if ( size > m_longestShip )
+    {
+        return QString();
+    }
+    return m_ships[size].shipsName();
+}
+
+bool BattleShipsConfiguration::multipleShips() const
+{
+    bool res = false;
+    for (unsigned int i=0; i<m_longestShip; i++)
+    {
+        res = res || (m_ships[i].number()>1);
+    }
+    return res;
+}
+
+bool BattleShipsConfiguration::isAValidConfiguration() const
+{
+    if ( m_longestShip==0 || m_boardHeight==0 || m_boardWidth==0 )
+    {
+        return false;
+    }
+    return true;
+}
+
+BattleShipsConfiguration BattleShipsConfiguration::defaultSingleShipsConfiguration(const bool allowAdjacent)
+{
+    BattleShipsConfiguration res(4, allowAdjacent, 10, 10);
+    return res.addShips(1, 1, QLatin1String("minesweepers"))
+              .addShips(2, 1, QLatin1String("frigates"))
+              .addShips(3, 1, QLatin1String("cruises"))
+              .addShips(4, 1, QLatin1String("carrier"));
+}
+
+BattleShipsConfiguration BattleShipsConfiguration::defaultMultipleShipsConfiguration(const bool allowAdjacent)
+{
+    BattleShipsConfiguration res(4, allowAdjacent, 10, 10);
+    return res.addShips(1, 4, QLatin1String("minesweepers"))
+              .addShips(2, 3, QLatin1String("frigates"))
+              .addShips(3, 2, QLatin1String("cruises"))
+              .addShips(4, 1, QLatin1String("carrier"));
+}
---
+++ src/ships.h
@@ -0,0 +1,72 @@
+/*
+  Copyright (c) 2013 Jaime Torres <jtamate@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
+#ifndef Ships_H
+#define Ships_H
+
+#include <QHash>
+#include <QString>
+#include "ship.h"
+
+class Ships
+{
+private:
+    unsigned int m_size;     // of the ships
+    unsigned int m_number;   // number of ships of this size
+    QString m_shipsName;     // the english name of the ships of this size
+public:
+    Ships(): m_size(0), m_number(0), m_shipsName() { }
+    Ships(unsigned int size, unsigned int number, QString& shipsName);
+    inline unsigned int size() const { return m_size; }
+    inline unsigned int number() const { return m_number; }
+    inline QString shipsName() const { return m_shipsName; }
+};
+
+// This configuration is interchanged in the network games.
+// In the versions previous to KDE SC 4.13, only adjacent and multiple where interchanged.
+class BattleShipsConfiguration
+{
+private:
+    unsigned int m_longestShip;
+    bool m_allowAdjacentShips;
+    unsigned int m_boardWidth;
+    unsigned int m_boardHeight;
+    QHash<unsigned int,Ships> m_ships;
+public:
+
+    BattleShipsConfiguration();
+    explicit BattleShipsConfiguration(unsigned int longestShipSize, const bool allowAdjacentShips, const unsigned int boardWidth, const unsigned int boardHeight);
+    BattleShipsConfiguration(const BattleShipsConfiguration& copy);
+    // does not add any ship longer than longestShip
+    // overwrites any previous configuration for ships of the requested size
+    BattleShipsConfiguration& addShips(unsigned int size, unsigned int number, QString shipsName);
+    BattleShipsConfiguration& addShips(Ships &ships);
+    unsigned int numberOfShipsOfSize(unsigned int size) const; // 0 if size is invalid
+    QString nameOfShipsOfSize(unsigned int size) const; // QString() if size is invalid
+    bool multipleShips() const; // return true if any ship size has more than one ship
+
+    void setLongestShipSize(unsigned int longestShipSize);
+    void setAllowAdjacentShips(const bool allow) { m_allowAdjacentShips = allow; }
+    void setBoardWidth(const unsigned int boardWidth) { m_boardWidth = boardWidth; }
+    void setBoardHeight(const unsigned int boardHeight) { m_boardWidth = boardHeight; }
+    inline unsigned int boardWidth() const { return m_boardWidth; }
+    inline unsigned int boardHeight() const { return m_boardHeight; }
+    inline bool isAllowedAdjacentShips() const { return m_allowAdjacentShips; }
+    inline unsigned int longestShip() const { return m_longestShip; }
+    
+    bool isAValidConfiguration() const;
+
+    static BattleShipsConfiguration defaultSingleShipsConfiguration(const bool allowAdjacent);
+    static BattleShipsConfiguration defaultMultipleShipsConfiguration(const bool allowAdjacent);
+
+};
+
+
+
+#endif // Ships_H
